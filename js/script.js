4fe51244e4cf78984eda539efa0afcdd95d96489
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2020-08-20 14:20:25 +0700

    melakukan edit waktu dengan vanilla js pada digital clock

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const hr = document.getElementById("hr");
-const mn = document.getElementById("mn");
-const sc = document.getElementById("sc");
-
 setInterval(() => {
+  // analog clock
+  const hr = document.getElementById("hr");
+  const mn = document.getElementById("mn");
+  const sc = document.getElementById("sc");
+
   let day = new Date();
   let hh = day.getHours() * 30;
   let mm = day.getMinutes() * 6;
@@ -11,4 +12,17 @@ setInterval(() => {
   hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
   mn.style.transform = `rotateZ(${mm}deg)`;
   sc.style.transform = `rotateZ(${ss}deg)`;
+
+  // digital clock
+  const hour = document.getElementById("hour");
+  const minutes = document.getElementById("minutes");
+  const seconds = document.getElementById("seconds");
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  hour.innerHTML = h + ":";
+  minutes.innerHTML = m + ":";
+  seconds.innerHTML = s;
 });
